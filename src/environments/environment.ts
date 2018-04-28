d2853ef9f44a13e11a6f09b3babdcde5428c39c8
100318656 <100318656@alumnos.uc3m.es>
2018-04-28 18:52:30 +0200

    mockups y requisitos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {        // Datos para vincular Firebase
     production: false,
     firebase: {
-        apiKey: "AIzaSyBxXs6W3RBmtqH0665dUgVIBKWL6t0geA8",
-        authDomain: "practica-final-dyepi.firebaseapp.com",
-        databaseURL: "https://practica-final-dyepi.firebaseio.com",
-        projectId: "practica-final-dyepi",
-        storageBucket: "practica-final-dyepi.appspot.com",
-        messagingSenderId: "736822958755"
+        apiKey: "AIzaSyBgs4VGEbtGx9gx1EYiLH-VwrIwlXlcARk",
+        authDomain: "proyecto-dyepi.firebaseapp.com",
+        databaseURL: "https://proyecto-dyepi.firebaseio.com",
+        projectId: "proyecto-dyepi",
+        storageBucket: "proyecto-dyepi.appspot.com",
+        messagingSenderId: "1051942072633"
     }
 };
