6b08fcf34c82686c783b8004238d06aef367c7e2
Daniel <100315290@alumnos.uc3m.es>
2018-05-09 12:42:49 +0200

    mapa funcionando

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MapaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-mapa',
@@ -15,11 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapaPage {
 
+  plantas: Array<{ name: string, url: string }>;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams
   ) {
-    
+    this.plantas = [
+      { name: 'Planta 0', url: "../../assets/imgs/planta0.png" },
+      { name: 'Planta 1', url: "../../assets/imgs/planta1.png" },
+      { name: 'Planta 2', url: "../../assets/imgs/planta2.png" },
+      { name: 'Planta 3', url: "../../assets/imgs/planta3.png" },
+    ];
+
   }
 
   ionViewDidLoad() {
